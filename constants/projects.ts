7f33805a055d44fea0db2d29dd1a734682ad69f1
pranoymajumdar <189314177+pranoymajumdar@users.noreply.github.com>
2025-02-02 13:38:31 +0530

    Added acora

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,34 +1,13 @@
 export const featuredProjects = [
-    {
-        id: 1,
-      title: 'TopStudz',
-      description: 'Modern student study management platform',
-      link: '#',
-      tech: ['Next.js', 'TypeScript', 'Tailwind'],
-    },
-    {
-        id: 2,
-      title: 'Oxabags',
-      description: 'Ecommerce site developed with Next.js, Sanity.io, and Razorpay',
-      link: '#',
-      tech: ['Next.js', 'Sanity.io', 'Razorpay'],
-    },
-    {
-        id: 3,
-      title: 'Hack Track',
-      description: 'Team collaboration project tracker built with Next.js, Shadcn/UI, and Supabase',
-      link: '#',
-      tech: ['Next.js', 'Supabase', 'Shadcn UI'],
-    },
-    {
-        id: 4,
-      title: 'GLA App Reimagined',
-      description: 'Prototype app for GLA University built with React Native',
-      link: '#',
-      tech: ['React Native', 'Expo', 'TypeScript'],
-    },
-  ];
+  {
+    id: 1,
+    title: "Acora",
+    description: "Single vendor ecommerce site developed with Next.js, Prisma and Razorpay.",
+    link: "#",
+    tech: ["Next.js", "Prisma", "Razorpay"]
+  }
+];
 
-export const featuredProjectSectionTitle = "Featured Projects"
-export const featuredProjectSectionDescription = "Some things I've built"
-export const featuredProjectSectionLink = "/projects"
+export const featuredProjectSectionTitle = "Featured Projects";
+export const featuredProjectSectionDescription = "Some things I've built";
+export const featuredProjectSectionLink = "/projects";
